3c124b27d4d71b81c8b0d67b70311f25a4b9ec81
Viacheslav-ua <75425740+Viacheslav-ua@users.noreply.github.com>
2021-07-06 22:54:49 +0300

    task-01

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const categoriesEl = document.querySelector('#categories');
+console.log(`В списке ${categoriesEl.childElementCount} категории.`);
+
+const itemsEl = document.querySelectorAll('.item');
+itemsEl.forEach(item => {
+    console.log('Категория:', item.firstElementChild.textContent);
+    console.log('Количество элементов: ', item.lastElementChild.childElementCount);
+});
