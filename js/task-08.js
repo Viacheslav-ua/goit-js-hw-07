fa3e34b9f5fb9f1c77a59dbc6e0ea52787896fd7
Viacheslav-ua <75425740+Viacheslav-ua@users.noreply.github.com>
2021-07-10 22:07:32 +0300

    task-08

---
+++ js/task-08.js
@@ -0,0 +1,42 @@
+function randomRGB() {
+    return `${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}`
+}
+
+function createBoxes(amount) {
+    let size = 30;
+    const boxesFragment = new DocumentFragment();
+
+    for (let i = 0; i < amount; i += 1) {
+        const boxEl = document.createElement('div');
+        boxEl.style.width = size + 'px';
+        boxEl.style.height = size + 'px';
+        boxEl.style.backgroundColor = `rgb(${randomRGB()})`
+        boxesFragment.appendChild(boxEl);
+        size += 10;
+    }
+    return boxesFragment
+}
+
+function destroyChild(parent) {
+    if (parent.children.length === 0) return;
+    const countElements = parent.children.length;
+    for (let i = 0; i < countElements; i += 1) {
+        parent.removeChild(parent.children[0]);
+    }
+}
+
+const refs = {
+    boxes: document.querySelector('#boxes'),
+    inputQty: document.querySelector('#controls input'),
+    renderBtn: document.querySelector('[data-action="render"]'),
+    destroyBtn: document.querySelector('[data-action="destroy"]'),
+};
+
+refs.renderBtn.addEventListener('click', () => {
+    destroyChild(refs.boxes);
+    refs.boxes.appendChild(createBoxes(refs.inputQty.value));
+});
+
+refs.destroyBtn.addEventListener('click', () => {
+    destroyChild(refs.boxes);
+});
